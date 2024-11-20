815936efbb9af2135a7e7341e7e4fd52ee5f7ff6
Matthew Rankin <matthew@questrail.com>
2024-11-20 06:21:04 -0700

    Update ver and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ This file contains all notable changes to the [tektronix][] project.
 
 ## Unreleased
 
+## v0.2.0 - 20-Nov-24
+
+- Added code and test for ver 1 of the RSA500 CSV file format.
+
 ## v0.1.0 - 30-Oct-23
 
 - Initial release for RSA500
--- src/tektronix/__init__.py
+++ src/tektronix/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013-2023 The tektronix developers. All rights reserved.
+# Copyright (c) 2013-2024 The tektronix developers. All rights reserved.
 # Project site: https://github.com/questrail/tektronix
 # Use of this source code is governed by a MIT-style license that
 # can be found in the LICENSE.txt file for the project.
-"""Package initialization.
-"""
-__version__ = "0.1.2"
+"""Package initialization."""
+
+__version__ = "0.2.0"
