1c1a93c40999c0443721fdea572d9f8c28e8fe0d
Matthew Rankin <matthew@questrail.com>
2024-11-20 06:19:09 -0700

    Add code and tests for ver 1 CSV file

--- src/tektronix/rsa500.py
+++ src/tektronix/rsa500.py
@@ -3,16 +3,21 @@
 # Project site: https://github.com/questrail/tektronix
 # Use of this source code is governed by a MIT-style license that
 # can be found in the LICENSE.txt file for the project.
-"""Read a CSV file saved by a RSA500 Spectrum Analyzer
-"""
+"""Read a CSV file saved by a RSA500 Spectrum Analyzer"""
 
 # Standard module imports
 import csv
+import logging
 import os
 import sys
 
 # Data analysis related imports
 import numpy as np
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO, format=" %(asctime)s -  %(levelname)s - %(message)s"
+)
 
 
 def read_csv_file(filename):
@@ -21,21 +26,37 @@ def read_csv_file(filename):
     infile = open(filename, "r", newline="", encoding="utf8")
 
     with infile as csvfile:
-        reader = csv.reader(
-            (line.replace("\0", "") for line in csvfile), delimiter=","
-        )
+        reader = csv.reader((line.replace("\0", "") for line in csvfile), delimiter=",")
         header["file"] = os.path.basename(filename)
         temp_row = next(reader)
-        if temp_row[0] != "Spectrum":
+        if temp_row[0] == "Spectrum":
+            ver = 0
+        elif temp_row[0] == "Spectrum 1":
+            ver = 1
+        else:
+            logging.error("Unknown csv filetype: %s", temp_row[0])
             sys.exit(1)
+        logging.info("RSA500 CSV ver %s", ver)
         header["timestamp"] = temp_row[1]
-        temp_row = next(reader)
-        temp_row = next(reader)
-        header["center_freq"] = float(temp_row[1])
-        header["center_freq_units"] = temp_row[2]
-        temp_row = next(reader)
-        header["ref_level"] = float(temp_row[1])
-        header["ref_level_units"] = temp_row[2]
+        if ver == 0:
+            temp_row = next(reader)
+            temp_row = next(reader)
+            header["center_freq"] = float(temp_row[1])
+            header["center_freq_units"] = temp_row[2]
+            temp_row = next(reader)
+            header["ref_level"] = float(temp_row[1])
+            header["ref_level_units"] = temp_row[2]
+        else:
+            # Read until we find the Center Frequency.
+            while True:
+                temp_row = next(reader)
+                if len(temp_row) == 3 and temp_row[0] == "Center Frequency":
+                    break
+            header["center_freq"] = float(temp_row[1])
+            header["center_freq_units"] = temp_row[2]
+            temp_row = next(reader)
+            header["ref_level"] = float(temp_row[1])
+            header["ref_level_units"] = temp_row[2]
 
         # Read until we find the [Parameters] section.
         while True:
--- tests/test_rsa500.py
+++ tests/test_rsa500.py
@@ -3,8 +3,8 @@
 # Project site: https://github.com/questrail/tektronix
 # Use of this source code is governed by a MIT-style license that
 # can be found in the LICENSE.txt file for the project.
-"""Unit tests for tektronix/rsa500.py.
-"""
+"""Unit tests for tektronix/rsa500.py."""
+
 import unittest
 import logging
 
@@ -23,12 +23,11 @@ logging.basicConfig(
 )
 
 
-class TestReadingCSVFiles(unittest.TestCase):
-    """Test reading an RSA500 csv data file."""
+class TestReadingCSVVer0Files(unittest.TestCase):
+    """Test reading an RSA500 csv ver 0 data file."""
 
     def setUp(self):
         test_csv_file = Path(TEST_DIR, "sample_data", "1M-11M.csv")
-        print(test_csv_file)
         (self.header, self.data) = rsa500.read_csv_file(test_csv_file)
 
     def test_header_when_reading_csv_file(self):
@@ -60,6 +59,45 @@ class TestReadingCSVFiles(unittest.TestCase):
             (self.data["amplitude"][1], 40.868099212646484),
             (self.data["amplitude"][-2], 17.863529205322266),
             (self.data["amplitude"][-1], 20.075450897216797),
+        ]
+        for actual, desired in amp_data:
+            npt.assert_almost_equal(actual, desired)
+
+
+class TestReadingCSVVer1Files(unittest.TestCase):
+    """Test reading an RSA500 csv ver 1 data file."""
+
+    def setUp(self):
+        test_csv_file = Path(
+            TEST_DIR, "sample_data", "Cab 200k-30M Monopole 10kHz TN2.csv"
+        )
+        (self.header, self.data) = rsa500.read_csv_file(test_csv_file)
+
+    def test_header_when_reading_csv_file(self):
+        """Test reading the header info of an RSA500 csv data file."""
+        self.assertEqual(self.header["file"], "Cab 200k-30M Monopole 10kHz TN2.csv")
+        self.assertEqual(self.header["center_freq"], 15_100_000.0)
+        self.assertEqual(self.header["span_freq"], 29_800_000.0)
+        self.assertEqual(self.header["resolution_bw"], 10_000.0)
+        self.assertEqual(self.header["resolution_bw_units"], "Hz")
+        self.assertEqual(self.header["num_traces"], 1)
+        self.assertEqual(self.header["num_points"], 2401)
+
+    def test_data_when_reading_csv_file(self):
+        """Test reading the tract data of an RSA500 csv data file."""
+        self.assertEqual(self.data.shape, (2401,))
+        self.assertEqual(self.data["amplitude"].shape, (2401,))
+        freq_data = [
+            (self.data["frequency"][0], 200_000.0),
+            (self.data["frequency"][3], 237_250.0),
+        ]
+        for actual, desired in freq_data:
+            npt.assert_almost_equal(actual, desired)
+        amp_data = [
+            (self.data["amplitude"][0], 82.783210754394531),
+            (self.data["amplitude"][1], 86.962478637695313),
+            (self.data["amplitude"][-2], 43.837810516357422),
+            (self.data["amplitude"][-1], 43.746368408203125),
         ]
         for actual, desired in amp_data:
             npt.assert_almost_equal(actual, desired)
